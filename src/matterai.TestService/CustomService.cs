5455aff1304266d6743808fc734efe2d14d958ca
Vladimir Vlasyuk <v.vlasyuk@corp.vk.com>
2022-01-23 17:02:19 +0300

    Added simple service

---
+++ src/matterai.TestService/CustomService.cs
@@ -0,0 +1,11 @@
+﻿namespace matterai.TestService
+{
+    public class CustomService
+    {
+        public CustomService()
+        {
+        }
+
+        public string GetGreetings(string name) => $"Hey, {name}";
+    }
+}
